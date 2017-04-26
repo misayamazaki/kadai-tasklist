f9f9ce4892aa282e613a150692d31634ac6a8b50
Misa Yamazaki <misa.189.y@gmail.com>
2017-04-26 08:14:12 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,72 +1,58 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
   before_action :require_user_logged_in
   before_action :correct_user, only: [:destroy]
-  
-    def index
-    @tasks = Task.all.page(params[:page])
-    end
     
-    def show
+  def show
     @task = Task.find(params[:id])
-    end
+    
+  end
 
-    def new
-    @task = Task.new
-    end
-
-    def create
-    @task = Task.new(task_params)
+  def create
     @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to root_url
-    else
-      @task = current_user.tasks.order('created_at DESC').page(params[:page])
-      flash.now[:danger] = 'Task が投稿されませんでした'
+    flash[:success] = 'メッセージを投稿しました。'
+    redirect_to root_url
+  else
+    @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
       render 'toppages/index'
-      
-    end
-    end
+    end 
+  end
 
-    def edit
-    @task = Task.find(params[:id])
-    end
+  def edit
+   @task = Task.find(params[:id])
+  end
 
-    def update
-    @task = Task.find(params[:id])
-
+   
+  def update
+    @task = current_user.tasks.find(params[:id])
     if @task.update(task_params)
-      flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
-    else
-      flash.now[:danger] = 'Task は更新されませんでした'
-      render :edit
+    flash[:success] = 'Task は正常に更新されました'
+    redirect_to @task
+  else
+    flash.now[:danger] = 'Task は更新されませんでした'
+    render :edit
     end
-    end
+  end
 
-    def destroy
-     @task.destroy
-
-     flash[:success] = 'Task は正常に削除されました'
-     redirect_back(fallback_location: root_path)
-    end
+  def destroy
+    @task.destroy
+    flash[:success] = 'Task を削除しました'
+    redirect_to root_path
+  end
     
-    private
+   
+  private
     
-    def set_task
-    @task = Task.find(params[:id])
-    end
-    
-    def task_params
-     params.require(:task).permit(:content, :status)
-    end
-    
-    
-    def correct_user
+  def task_params
+    params.require(:task).permit(:content, :user)
+  end
+   
+  def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to root_path
+    redirect_to root_path
     end
-    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
     @tasks = @user.tasks.order('created_at DESC').page(params[:page])
     counts @user
   end
-
   def new
     @user = User.new
   end
@@ -26,6 +25,27 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  
+  def update
+    @task = Task.find(params[:id])
+
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
   private
 
   def user_params
